bbe20155b4a2d82bf74acdf0362c8b381906209f
Kevin Harrington <mad.hephaestus@gmail.com>
2017-08-21 18:28:33 -0400

    renamed temp constants

--- src/coms/PidConfigServer.cpp
+++ src/coms/PidConfigServer.cpp
@@ -4,11 +4,11 @@ void PidConfigServer::event(float * buffer){
 
   for(int i=0; i<myPumberOfPidChannels;i++){
     // read values from the packet buffer
-    float p        = buffer[(i*3)+0];
-    float i  = buffer[(i*3)+1];
-    float d     = buffer[(i*3)+2];
+    float kpn        = buffer[(i*3)+0];
+    float kin  = buffer[(i*3)+1];
+    float kdn     = buffer[(i*3)+2];
     // Set the PID constants
-    myPidObjects[i]->setPIDConstants(p, i, d);
+    myPidObjects[i]->setPIDConstants(kpn, kin, kdn);
 
   }
 
