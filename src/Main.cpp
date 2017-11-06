2a8d5c8fe44966c6315529b2915c0c2ba1d7d3a9
ngoldfarb <nagoldfarb@wpi.edu>
2017-11-05 23:01:04 -0500

    fixed small error

--- src/Main.cpp
+++ src/Main.cpp
@@ -33,11 +33,11 @@ int main() {
 #else
    SPI * spiDev = new SPI(MOSI, MISO, CLK);
    pid[0] = new PIDimp( new Servo(SERVO_1, 5),
-                         new AS5050(spiDev, ENC_1, new AnalogIn(LOAD_1)));  // mosi, miso, sclk, cs
+                         new AS5050(spiDev, ENC_1),new AnalogIn(LOAD_1));  // mosi, miso, sclk, cs
    pid[1] = new PIDimp( new Servo(SERVO_2, 5),
-                         new AS5050(spiDev, ENC_2,new AnalogIn(LOAD_2)));  // mosi, miso, sclk, cs
+                         new AS5050(spiDev, ENC_2),new AnalogIn(LOAD_2));  // mosi, miso, sclk, cs
    pid[2] = new PIDimp( new Servo(SERVO_3, 5),
-                         new AS5050(spiDev, ENC_3,new AnalogIn(LOAD_3)));  // mosi, miso, sclk, cs
+                         new AS5050(spiDev, ENC_3),new AnalogIn(LOAD_3));  // mosi, miso, sclk, cs
 #endif
 
    // Invert the direction of the motor vs the input
